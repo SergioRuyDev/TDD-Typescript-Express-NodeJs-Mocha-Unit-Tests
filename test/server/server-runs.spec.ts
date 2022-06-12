ecbb8f7820090b22721b8adc98290a5e44eaf726
SergioRuyDev <sergioruyenator@gmail.com>
2022-06-12 13:34:07 -0300

    first test pass.

--- test/server/server-runs.spec.ts
+++ test/server/server-runs.spec.ts
@@ -1,5 +1,12 @@
+import request from "supertest";
+import { expect } from "chai";
+
+import createServer from "server";
+const app = createServer();
+
+
 describe("server checks", function() {
-    it('server is created without error', function () {
-        console.log("Hi")
+    it('server is created without error', function (done) {
+        request(app).get("/").expect(200, done);
     });
 });
